9f07f52a14adcaf1e560f702eeb9fcd57e6fa77a
hwangtate <ghkdxodud10253504@gmail.com>
2023-10-22 10:46:16 +0900

    home 모바일 크기 조정

--- src/footer.js
+++ src/footer.js
@@ -6,7 +6,7 @@ function Footer (){
       <p><img src="img/logo1.png" id="footerlogo" alt="푸터 로고" /></p>
       <p id="footertext">
         업체명 : 레스다인 주소 : 세종특별자치시 달빛1로 206<br />
-        TEL. 01046883504 사업자등록번호 : 000000000 개인정보책임자 : 황태영<br />
+        TEL. 01046883504 사업자등록번호 : 000000000 <br /> 개인정보책임자 : 황태영 <br /><br />
         Copyright 2023. RESDINE all rights reserved.
       </p>
       <p id="footertext">
--- src/home.js
+++ src/home.js
@@ -11,6 +11,11 @@ function Home() {
         매출이 증가하지<br />
         않는다면 100% 환불해드립니다.
       </h1>
+      <h1 id="mobile-title1">
+        매출이 증가하지<br />
+        않는다면 100%<br />
+        환불해드립니다.
+      </h1>
       <p className="p" id="p">
         먼저 성공한 사람들의 성공 가이드
       </p>
@@ -23,12 +28,12 @@ function Home() {
         <Link to={'/'} className="contain2">
           <img src="img/Document.png" className="icon" alt="아이콘 2" />
           <p className="text">컨설턴트 커리어 & 소개 </p>
-          <p className="subtext">요식업 및 외식업으로 이미 성공한<br />사업가에게 컨설팅을 받으세요.</p>
+          <p className="subtext">이미 성공한 사업가에게<br />컨설팅을 받으세요.</p>
         </Link>
         <Link to={'/'} className="contain2">
           <img src="img/Analyze.png" className="icon" alt="아이콘 3" />
           <p className="text">전략 컨설팅 & 창업 컨설팅</p>
-          <p className="subtext">성공한 요식업 및 외식업<br />사업가의 노하우와 비밀을 받아가세요.</p>
+          <p className="subtext">성공한 사업가의<br />노하우와 비밀을 받아가세요.</p>
         </Link>
       </div>
     </main>
